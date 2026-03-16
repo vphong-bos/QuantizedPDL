aa51e3963bbb4a17744bf12c7e9e72d5167cf4c5
vphong-bos <phong.vu@bos-semi.com>
2026-03-16 17:10:35 +0000

    remove download in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,11 +9,6 @@ WEIGHTS_DIR="${SCRIPT_DIR}/weights"
 WEIGHTS_FILE="${WEIGHTS_DIR}/model_final_bd324a.pkl"
 WEIGHTS_URL="https://dl.fbaipublicfiles.com/detectron2/PanopticDeepLab/Cityscapes-PanopticSegmentation/panoptic_deeplab_R_52_os16_mg124_poly_90k_bs32/model_final_bd324a.pkl"
 
-CITYSCAPES_DIR="${SCRIPT_DIR}/data/cityscapes"
-
-LEFTIMG_ZIP="${SCRIPT_DIR}/leftImg8bit_trainvaltest.zip"
-GTFINE_ZIP="${SCRIPT_DIR}/gtFine_trainvaltest.zip"
-
 echo "Installing Python requirements..."
 python -m pip install -q -r "${REQ_FILE}"
 
@@ -27,21 +22,5 @@ if [ ! -f "${WEIGHTS_FILE}" ]; then
     echo "Downloading Panoptic-DeepLab weights..."
     curl -L "${WEIGHTS_URL}" -o "${WEIGHTS_FILE}"
 fi
-
-echo "Preparing Cityscapes dataset directory..."
-mkdir -p "${CITYSCAPES_DIR}"
-
-echo "Extracting Cityscapes datasets..."
-
-unzip -q -o "${LEFTIMG_ZIP}" -d "${CITYSCAPES_DIR}"
-unzip -q -o "${GTFINE_ZIP}" -d "${CITYSCAPES_DIR}"
-
-export CITYSCAPES_DATASET="${CITYSCAPES_DIR}"
-
-echo "Generating labelTrainIds..."
-python -m cityscapesscripts.preparation.createTrainIdLabelImgs
-
-echo "Cityscapes dataset path:"
-echo "${CITYSCAPES_DIR}"
 
 echo "Setup complete."
